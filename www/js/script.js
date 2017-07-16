6fec040bd3a62f977d9e2b55435e0291f36adb7e
jethro <jethro@kayweb.com.au>
2017-07-16 20:59:54 +0800

    geocoding

--- www/js/script.js
+++ www/js/script.js
@@ -19,7 +19,40 @@ document.addEventListener('init', function(event) {
     $('.continue-as-regular-user').click(function(){
        window.location.href = "main.html"; 
     });
+    
+    $(".add-crime").click(function(){
+        var center = map.getCenter();
+        var mode = $(this).attr('data-mode');
+        var address = '';
+        gmgeocoder.geocode({'latLng': center}, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                if (results[0]) {
+                    if(results[0].formatted_address.length>64){
+                        address = results[0].formatted_address.substring(0,64)+'...';
+                        add_crime(center,address,mode);
+                    }
+                    else {
+                        address = results[0].formatted_address;
+                        add_crime(center,address,mode);
+                    }
+                }                   
+            }
+            else {
+                address = "Geocoder not possible";
+                add_crime(center,address,mode);
+            }
+        });
+    });
 });
+
+var add_crime = function(latlong,address,mode)
+{
+//    modes:
+//            1 - Create blotter
+//            2 - Report Crime
+            
+    alert(address)
+};
 
 var message = function(msg)
 {
