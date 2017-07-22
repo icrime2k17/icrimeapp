6573e225b4aa7cc28b902f97c781344ecd3b7d1c
icrime2k17 <icrime2k17@gmail.com>
2017-07-22 14:28:26 +0900

    live config

--- www/js/config.js
+++ www/js/config.js
@@ -1,3 +1,7 @@
+// var config = {
+//     url : 'http://192.168.43.59/icrime/index.php/webservice'
+// };
+
 var config = {
-    url : 'http://192.168.43.59/icrime/index.php/webservice'
+    url : 'http://icrime2k17.000webhostapp.com/webservice'
 };
